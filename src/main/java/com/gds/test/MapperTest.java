37df4c7a541385fa8258d253a7ee61d66204d814
gongdaosong <308227363@qq.com>
2019-09-23 09:47:36 +0800

    重新发请求修改123

--- src/main/java/com/gds/test/MapperTest.java
+++ src/main/java/com/gds/test/MapperTest.java
@@ -77,7 +77,7 @@ public class MapperTest {
         EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
         for (int i = 0; i<1000; i++){
             String uid = UUID.randomUUID().toString().substring(0, 5)+i;
-            mapper.insertSelective(new Employee(null,uid,"M",uid+"@qq.com",3));
+            mapper.insertSelective(new Employee(null,uid,"F",uid+"@qq.com",6));
         }
         System.out.println("批量生成完成!");
 
